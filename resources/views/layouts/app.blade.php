cc7d30ab1db67a577d025a86db4db06691b3c227
shaevmv <ShaevMV@gmail.com>
2023-03-15 21:36:26 +0300

    final fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        <link href="/img/favicon.png" rel="apple-touch-icon">
         <title>Оформить дружеский билет</title>
 
         <!-- Fonts -->
